6d389364b3b99904b3a1e67a16d484024708a029
Simon Nonnis <simononnis@gmail.com>
2016-07-31 14:32:22 +0200

    'Update'

--- src/test/empty.test.js
+++ src/test/empty.test.js
@@ -1,14 +1,1 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { expect } from 'chai';
 
-import HotelData from '../components/HotelData';
-
-describe('<HotelData/>', () => {
-
-  it('should have props for: Name, Stars, Facilities', () => {
-    const wrapper = shallow(<HotelData/>);
-
-    expect(wrapper.props().hotel).to.be.defined;
-})
-});
